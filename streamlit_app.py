03a36d076097ad98a5cf709b2843f34bf049dfe1
maxrcode-source <159390701+maxrcode-source@users.noreply.github.com>
2024-02-14 20:14:45 +0000

    Change Title

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,24 +5,10 @@ import streamlit as st
 import smtplib
 
 
-st.title('Welcome to the _**SHS Computer Science Site**_!')
+st.title('SHS Computer Science Site')
 st.header('This brand new site, coded in Python with hosting/library provided by _Streamlit.io_, is for all things Computer Science within Sahuarita High School, this website will provide resources for things such as the CS TSA Test.')
 
 st.divider()
 
 suggestions = st.text_input('Have any suggestions? Feedback such as bugs or even suggestions can help make the site better!')
 
-if suggestions != "":
-
-  # creates SMTP session
-  s = smtplib.SMTP('smtp.gmail.com', 587)
-  # start TLS for security
-  s.starttls()
-  # Authentication
-  s.login("max.ramirez@susdlearns.us", "302306SUSDD")
-  # message to be sent
-  message = "Message_you_need_to_send"
-  # sending the mail
-  s.sendmail("max.ramirez@susdlearns.us", "max.ramirez@susdlearns.us", message)
-  # terminating the session
-  s.quit()
