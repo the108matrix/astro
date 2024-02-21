456f925d26e89833e86e2b0943ae9b10406329aa
Murat Beyhan <44893123+muratbeyhan@users.noreply.github.com>
2024-02-21 10:08:21 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import numpy as np
 DATA_PATH = 'turkey_earthquakes.csv'
 
 def load_data():
-    data = pd.read_csv(DATA_PATH, sep=';')
+    data = pd.read_csv(DATA_PATH, sep=',')
     return data
 
 df = load_data()
