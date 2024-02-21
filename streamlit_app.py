50da3c061563199aa7d29a9a8f19bacfe5244d4d
Murat Beyhan <44893123+muratbeyhan@users.noreply.github.com>
2024-02-21 21:26:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,3 +90,17 @@ with col2:
     # Plotting
     tab4.subheader("Chart")
     tab4.bar_chart(deprem_df.set_index('DateOnly'))
+
+import folium
+import streamlit as st
+
+from streamlit_folium import st_folium
+
+# center on Liberty Bell, add marker
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+folium.Marker(
+    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+).add_to(m)
+
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)
