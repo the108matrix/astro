e2998815f5271ab184937c389a3381b8ca952cff
Murat Beyhan <44893123+muratbeyhan@users.noreply.github.com>
2024-02-22 14:24:34 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,30 +76,30 @@ with col2:
         center_lon = filtered_df['longitude'].mean()
         
         # Define magnitude ranges
-        magnitude_ranges = [(0, 3), (3.1, 4), (4.1, 5), (5.1, 6), (6.1, 7), (7.1, 8), (8.1, 9),(9.1,float('inf'))]
+        magnitude_ranges = [(0, 2.9), (3.0, 3.9), (4.0, 4.9), (5.0, 5.9), (6.0, 6.9), (7.0, 7.9), (8.0, 8.9),(9.0,float('inf'))]
         
         # Set colors for each magnitude range
         colors = ["yellow", "lightgreen", "blue", "orange", "red", "purple", "brown", "black"]
-        mag_labels =["0.0-3.0", "3.1-4.0", "4.1-5.0", "5.1-6.0", "6.1-7.0", "7.1-8.0", "8.1-9.0","9.1<"]
+        mag_labels =["0.0-2.9", "3.0-4.9", "4.0-4.9", "5.0-5.9", "6.0-6.9", "7.0-7.9", "8.0-8.9","9.0<"]
         
         # Create a column to store color information based on Magnitude ranges
         filtered_df['Color'] = pd.cut(filtered_df['Magnitude'], bins=[range[0] for range in magnitude_ranges] + [float('inf')], labels=mag_labels, right=False)
         # Define a color palette for each magnitude range
         #color_palette = dict(zip(mag_labels, colors))
         color_palette = {
-            "0.0-3.0": "yellow",
-            "3.1-4.0": "lightgreen",
-            "4.1-5.0": "blue",
-            "5.1-6.0": "orange",
-            "6.1-7.0": "red",
-            "7.1-8.0": "purple",
-            "8.1-9.0": "brown",
-            "9.1<": "black"
+            "0.0-2.9": "yellow",
+            "3.0-3.9": "lightgreen",
+            "4.0-4.9": "blue",
+            "5.0-5.9": "orange",
+            "6.0-6.9": "red",
+            "7.0-7.9": "purple",
+            "8.0-8.9": "brown",
+            "9.0<": "black"
         }
         # Create scatter mapbox plot
         fig = px.scatter_mapbox(filtered_df, lat="latitude", lon="longitude", color="Color", color_discrete_map=color_palette,
                                 size="Magnitude", zoom=5, height=600, width=800,hover_name="Date",hover_data=["Magnitude"],
-                                category_orders={"Color": ["0.0-3.0", "3.1-4.0", "4.1-5.0", "5.1-6.0", "6.1-7.0", "7.1-8.0", "8.1-9.0", "9.1<"]})
+                                category_orders={"Color": ["0.0-2.9", "3.0-3.9", "4.0-4.9", "5.0-5.9", "6.0-6.9", "7.0-7.9", "8.0-8.9", "9.0<"]})
         
         
         fig.update_layout(mapbox_style="open-street-map")
@@ -108,7 +108,7 @@ with col2:
         fig.update_layout(mapbox_center=dict(lat=center_lat, lon=center_lon))  # Center the map
         
         # Define magnitude ranges
-        magnitude_ranges = [(0, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9),(9,float('inf'))]
+        magnitude_ranges = [(0, 2.9), (3.0, 3.9), (4.0, 4.9), (5.0, 5.9), (6.0, 6.9), (7.0, 7.9), (8.0, 8.9),(9.0,float('inf'))]
         
         # Count earthquakes in each magnitude range
         magnitude_counts = [filtered_df[(filtered_df['Magnitude'] >= min_mag) & (filtered_df['Magnitude'] < max_mag)].shape[0] for min_mag, max_mag in magnitude_ranges]
@@ -139,3 +139,4 @@ with col2:
     # Plotting
     tab4.subheader("Chart")
     tab4.bar_chart(deprem_df.set_index('DateOnly'))
+    
