5a7c1d765fe4acbb2611f8dafe89f09ec57d66b5
Murat Beyhan <44893123+muratbeyhan@users.noreply.github.com>
2024-02-22 12:08:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 
 # Load data
@@ -35,8 +36,8 @@ with col1:
 
     
     # Magnitude range selection
-    min_mag = st.number_input('Minimum Magnitude', min_value=0.0, max_value=10.0,value=5.0)
-    max_mag = st.number_input('Maximum Magnitude', min_value=0.0, max_value=10.0, value=9.0)
+    min_mag = st.number_input('Minimum Magnitude', min_value=0.0, max_value=10.0,value=5.0,step=0.1)
+    max_mag = st.number_input('Maximum Magnitude', min_value=0.0, max_value=10.0, value=9.0,step=0.1)
 
 # Filter data based on selected date range and magnitude range
 filtered_df = df[(df['DateOnly'] >= start_date) & (df['DateOnly'] <= end_date)]
@@ -55,20 +56,40 @@ with col2:
     # Show data checkbox
     if st.checkbox('Show data'):
         st.dataframe(df)
-  
+
+    # Show table checkbox
+    
     
     # Show selected data checkbox
-    tab1, tab2,tab3,tab4= st.tabs(["Map","🗃 Num.of EQs","🗃 Filtered Data","📈 Chart"])
+    tab1,tab2,tab3,tab4= st.tabs(["Map","🗃 Num.of EQs","🗃 Filtered Data","📈 Chart"])
+    #tab1,tab2,tab3,tab4,tab5= st.tabs(["Map","🗃 Num.of EQs","🗃 Filtered Data","📈 Chart","Mag. Distribution"])
     # Earthquake Map
+
     
+    # Draw the map with color-coded circles based on Magnitude
     tab1.subheader("Earthquake Map")
-    
+
     if not filtered_df.empty:
         #tab1.map(filtered_df, size=20, color='Color')
-        tab1.map(filtered_df, size=20, color='#00bb00')
-
-    else:
-        tab1.write("No earthquakes found in the selected range.")
+        #tab1.map(filtered_df, size=20, color='#00bb00')
+        fig = px.scatter_mapbox(filtered_df, lat="latitude", lon="longitude", color="Magnitude", size="Magnitude", zoom=5, height=600, width=800) # Ayarlanan boyutlar
+        fig.update_layout(mapbox_style="open-street-map")
+        fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+        tab1.plotly_chart(fig)
+        # Define magnitude ranges
+        magnitude_ranges = [(0, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9),(9,float('inf'))]
+        
+        # Count earthquakes in each magnitude range
+        magnitude_counts = [filtered_df[(filtered_df['Magnitude'] >= min_mag) & (filtered_df['Magnitude'] < max_mag)].shape[0] for min_mag, max_mag in magnitude_ranges]
+        
+        # Create a DataFrame for plotting
+        magnitude_df = pd.DataFrame({'Magnitude Range': ['{}-{}'.format(min_mag, max_mag) for min_mag, max_mag in magnitude_ranges], 'Count': magnitude_counts})
+        
+        # Plot the bar chart with plotly
+        fig = px.bar(magnitude_df, x='Magnitude Range', y='Count', text='Count')
+        fig.update_traces(texttemplate='%{text}', textposition='outside')
+        fig.update_layout(xaxis_tickangle=-45)
+        tab1.plotly_chart(fig)
         
     tab2.subheader("Number of Earthquake")
     tab2.write(deprem_df)
@@ -80,3 +101,6 @@ with col2:
     # Plotting
     tab4.subheader("Chart")
     tab4.bar_chart(deprem_df.set_index('DateOnly'))
+    
+    
+     
