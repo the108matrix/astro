cfea44e53a3b3b5a67bda982527b494f7712336e
maxrcode-source <159390701+maxrcode-source@users.noreply.github.com>
2024-04-04 10:27:44 -0700

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import smtplib
 
 st.title('SHS Computer Science Site')
 st.header('This brand new site, coded in Python with hosting/library provided by _Streamlit.io_, is for all things Computer Science within Sahuarita High School, this website will provide resources for things such as the CS TSA Test.')
-st.image('shslogo.png', caption='Sahuarita High School Mustang', width=800)
+st.image('shslogo.png', caption='Sahuarita High School Mustang', width=600)
 
 st.divider()
 
