1f7224b9d751e100757560ae03bbeda2e9a5de5a
maxrcode-source <159390701+maxrcode-source@users.noreply.github.com>
2024-04-04 10:14:39 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,8 @@ st.divider()
 
 suggestions = st.text_input('Have any suggestions? Feedback such as bugs or even suggestions can help make the site better!')
 
+st.write(suggestions)
+
 st.divider()
 
 st.write("This site is currently under construction, so please be patient with the site. Due to the limitations of the platform of the previous site, I have decided that it would be best to start fresh. I will be adding more features to the site as time goes on. Thank you.")
