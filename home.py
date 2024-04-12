c2ab3f039c9d1726caa5582984aa7d14e024122a
maxrcode-source <159390701+maxrcode-source@users.noreply.github.com>
2024-04-12 17:05:26 +0000

    NEW

--- home.py
+++ home.py
@@ -14,7 +14,7 @@ st.divider()
 st.header("Here is all of the available practice tests located below.")
 
 with st.expander("Full TSA Practice Test", expanded=False):
-  st.page_link("pages/ncpp.py", label="NUMBER CONVERSION PRACTICE PROGRAM")
+  st.page_link("ncpp.py", label="NUMBER CONVERSION PRACTICE PROGRAM")
 
                  
 
