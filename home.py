90c4f5fe8b0f6e4866688bcf97aeffdad1fb1c48
maxrcode-source <159390701+maxrcode-source@users.noreply.github.com>
2024-04-16 07:02:02 -0700

    Add files via upload

---
+++ home.py
@@ -0,0 +1,26 @@
+import altair as alt
+import numpy as np
+import pandas as pd
+import streamlit as st
+import smtplib
+
+
+st.title('SHS Computer Science Site')
+st.write('This brand new site, coded in Python with hosting/library provided by _Streamlit.io_, is for all things Computer Science within Sahuarita High School, this website will provide resources for things such as the CS TSA Test.')
+#st.image('shslogo.png', caption='Sahuarita High School Mustang', width=600)
+
+st.divider()
+
+st.header("Here is all of the available practice tests located below.")
+
+with st.expander("Full TSA Practice Test", expanded=False):
+  st.page_link("ncpp.py", label="NUMBER CONVERSION PRACTICE PROGRAM")
+
+                 
+
+            
+st.header("And here, is all of the available resources located below.")
+
+st.divider()
+
+st.write("This site is currently under construction, so please be patient with the site. Due to the limitations of the platform of the previous site, I have decided that it would be best to start fresh. I will be adding more features to the site as time goes on. Thank you.")
