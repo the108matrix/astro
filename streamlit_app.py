7d594e88884dc8979ebfd8506e26835d182be50f
Noemie <noemie.schweizer@epfl.ch>
2024-04-20 16:56:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ import streamlit as st
 """
 # Welcome to Streamlit!
 
+# Here are possible allergies right now:
+
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
